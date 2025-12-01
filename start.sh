7d80c55a364f5573dca7ba9008dcf1e3345d6f38
Bixente Grandjean <bixente.grandjean@gmail.com>
2025-12-01 17:55:15 +0100

    [*] Tweak start.sh

--- start.sh
+++ start.sh
@@ -1,16 +1,16 @@
 read -n1 -p "Detach container? [Y,n]" detach
 echo
 echo "docker build . -t sopapi"
-#docker build . -t sopapi
+docker build . -t sopapi
 
 case $detach in
   n|N)
     echo "docker compose up --remove-orphans";;
-    #docker compose up --remove-orphans;;
+    docker compose up --remove-orphans;;
   *)
     echo "docker compose up -d --remove-orphans";;
-    #docker compose up -d --remove-orphans;;
+    docker compose up -d --remove-orphans;;
 esac
 
 echo "system prune -a"
-#docker system prune -a
+docker system prune -a
